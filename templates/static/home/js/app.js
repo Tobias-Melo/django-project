a04a883936d1bd342ecb624b0bd4a8545fefb50e
Tobias-Melo <tobiasmellow@gmail.com>
2023-05-06 00:12:14 -0300

    steps

--- templates/static/home/js/app.js
+++ templates/static/home/js/app.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
           speed: 200,
           loop: true,
      })
-     .type("interativa!", {delay: 400})
-     .delete("interativa!".length)
-     .type("eficiente!", {delay: 400})
-     .delete("eficiente!".length)
-     .type("inovadora!", {delay: 400})
-     .delete("inovadora!".length)
-     .type("acessível!", {delay: 400})
-     .delete("acessível!".length)
-     .type("moderna!", {delay: 400})
+     .type("interativa.", {delay: 400})
+     .delete("interativa.".length)
+     .type("eficiente.", {delay: 400})
+     .delete("eficiente.".length)
+     .type("inovadora.", {delay: 400})
+     .delete("inovadora.".length)
+     .type("acessível.", {delay: 400})
+     .delete("acessível.".length)
+     .type("moderna.", {delay: 400})
      
      .go();
 });
