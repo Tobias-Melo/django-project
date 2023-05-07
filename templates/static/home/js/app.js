899325d6ae7427a9c2e45fdc737cec1dc4aca0ac
Tobias-Melo <tobiasmellow@gmail.com>
2023-05-07 13:06:17 -0300

    index html

--- templates/static/home/js/app.js
+++ templates/static/home/js/app.js
@@ -3,15 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
           speed: 200,
           loop: true,
      })
-     .type("interativa.", {delay: 400})
-     .delete("interativa.".length)
-     .type("eficiente.", {delay: 400})
-     .delete("eficiente.".length)
-     .type("inovadora.", {delay: 400})
-     .delete("inovadora.".length)
-     .type("acessível.", {delay: 400})
-     .delete("acessível.".length)
-     .type("moderna.", {delay: 400})
-     
-     .go();
+          .type("interativa.", { delay: 400 })
+          .delete("interativa.".length)
+          .type("eficiente.", { delay: 400 })
+          .delete("eficiente.".length)
+          .type("inovadora.", { delay: 400 })
+          .delete("inovadora.".length)
+          .type("acessível.", { delay: 400 })
+          .delete("acessível.".length)
+          .type("moderna.", { delay: 400 })
+
+          .go();
 });
+
+
+window.onscroll = function () {
+     var div = document.querySelector('.voltar-topo');
+     if (window.scrollY > 0) {
+          div.classList.add('mostrar');
+     } else {
+          div.classList.remove('mostrar');
+     }
+}
