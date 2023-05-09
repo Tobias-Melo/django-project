9fea95db7718dea7f66253ccabd147da09882ae2
Tobias-Melo <tobiasmellow@gmail.com>
2023-05-09 00:05:40 -0300

    users signin

--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == "GET":
+        return render(request, 'signup.html')
+    else:
+        name = request.POST.get('name')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            messages.error(request, 'Você já usou este e-mail. Mude para login ou digite um novo endereço de e-mail')
+            return render(request, 'signup.html')
+
+        return render(request, 'painel.html') 
 
 def login(request):
     return render(request, 'login.html')
